76d4379f62da2b677fd87f55212318132f6b759a
Programming With Diego <160330088+ProgrammingWithDiego@users.noreply.github.com>
2025-06-04 17:07:18 +0700

    Add Icons & Image (Cat)
    Icons at /src/components/navigation/icons
    
    Cat Image (Temporary User Profile)
    /public/UserProfile.jpg

---
+++ src/components/navigation/icons/HamburgerIcon.tsx
@@ -0,0 +1,19 @@
+const HamburgerIcon = ({ className = "w-6 h-6" }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className={`lucide lucide-menu-icon lucide-menu ${className}`}
+  >
+    <path d="M4 12h16" />
+    <path d="M4 18h16" />
+    <path d="M4 6h16" />
+  </svg>
+);
+export default HamburgerIcon;
